cf4ad047bf540c6caf7437c21f505c9e37f700b1
deepank84 <deepank84@gmail.com>
2016-09-28 08:49:28 -0400

    Added new changes

--- user-service/src/main/java/com/myweb/airline/controller/UserServiceController.java
+++ user-service/src/main/java/com/myweb/airline/controller/UserServiceController.java
@@ -2,6 +2,7 @@ package com.myweb.airline.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.cassandra.core.CassandraOperations;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,15 +21,16 @@ public class UserServiceController {
 	private CassandraOperations operations;
 
 	@RequestMapping(value = "/create", method = RequestMethod.POST)
-	public void createUser(@RequestBody User user) {
+	public ResponseEntity<Void> createUser(@RequestBody User user) {
 		operations.insert(user);
+		return ResponseEntity.noContent().build();
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public User getUser(@PathVariable String id) {
+	public ResponseEntity<User> getUser(@PathVariable String id) {
 		Select select = QueryBuilder.select().from("users");
 		Clause clause = QueryBuilder.eq("id", id);
 		select.where(clause);
-		return operations.selectOne(select, User.class);
+		return ResponseEntity.ok().body(operations.selectOne(select, User.class));
 	}
 }
